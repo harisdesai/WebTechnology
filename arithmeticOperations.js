404d23483ef294da38b973f7c39cf52317e1771b
harisdesai <harisdesai007@gmail.com>
2026-01-14 15:30:01 +0530

    day2

---
+++ arithmeticOperations.js
@@ -0,0 +1,29 @@
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Hello";
+// let str2 = " Haris";
+// console.log(str1 + str2);
+
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + "2")
+// console.log(1 + 3 + "2")
+
+//IMPORTANT NOTE - Javascript only automatically convert a string to a number in arithmetical operations
+
+// console.log(true)
+// console.log(+ true)
+// console.log(+ "true")
+// console.log(+ false)
+
+// let GameCounter = 100;
+// GameCounter++;
+// console.log(GameCounter);
+
+// let scoreCounter = 200
+// ++scoreCounter
+// console.log(scoreCounter)
---
+++ conversionOperation.js
@@ -0,0 +1,14 @@
+let num = 33;
+let stringNum = String(num);
+console.log(stringNum)
+console.log(typeof(stringNum));
+
+let string = "33";
+let numString = Number(string);
+console.log(numString);
+console.log(typeof(numString))
+
+
+/*Ativity 1
+# Do conversions of all data type
+*/
