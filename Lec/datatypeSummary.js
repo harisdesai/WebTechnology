4d4bf00012d0bf8896255fca226cadf22645e545
harisdesai <harisdesai007@gmail.com>
2026-01-20 10:44:53 +0530

    commited

---
+++ Lec/datatypeSummary.js
@@ -0,0 +1,31 @@
+//symbol example
+const anotherid = Symbol("123")
+console.log(typeof(anotherid))
+
+const id = 123
+console.log(id === anotherid)
+
+
+//js dynamic or static ans-dynamic
+
+
+//array
+const name = ['dikshant','tahir','pratik']
+const num = [1,2,3]
+
+
+//object
+const obj = {
+    fName : "JS",
+    age : 20
+}
+console.log(obj.age)
+
+
+//functions
+//activity - Deep study function
+function dikshant(){
+    console.log("Dikshant")
+}
+
+dikshant()
---
+++ string.js
@@ -0,0 +1,14 @@
+let str1 = "Dikshant"
+console.log(typeof(str1))
+
+//string interpolation
+console.log(`Hello my name is ${str1}`)
+
+const gameCount = new String("Dikshant")
+console.log(gameCount.length)
+
+console.log(gameCount.toLowerCase())
+console.log(gameCount.charAt(1))
+console.log(gameCount.indexOf('i'))
+
+//activity - all methods
